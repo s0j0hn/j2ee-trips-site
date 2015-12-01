4b8b2239df1a4a753db514a5652e787db069ce31
root <jan222@bitbucket.org>
2015-12-01 20:47:21 +0100

    Le debut

---
+++ src/com/suptrips/Trips.java
@@ -0,0 +1,102 @@
+/**
+ * "Visual Paradigm: DO NOT MODIFY THIS FILE!"
+ * 
+ * This is an automatic generated file. It will be regenerated every time 
+ * you generate persistence class.
+ * 
+ * Modifying its content may cause the program not work, or your work may lost.
+ */
+
+/**
+ * Licensee: 
+ * License Type: Evaluation
+ */
+package com.suptrips;
+
+import java.io.Serializable;
+import javax.persistence.*;
+@Entity
+
+@Table(name="Trips")
+public class Trips implements Serializable {
+	public Trips() {
+	}
+	
+	@Column(name="idtrip", nullable=false, length=11)	
+	@Id	
+	@GeneratedValue(generator="COM_SUPTRIPS_TRIPS_IDTRIP_GENERATOR")	
+
+	private int idtrip;
+	
+	@Column(name="depart_date", nullable=true)	
+	@Temporal(TemporalType.DATE)	
+	private java.util.Date depart_date;
+	
+	@Column(name="arrive_date", nullable=true)	
+	@Temporal(TemporalType.DATE)	
+	private java.util.Date arrive_date;
+	
+	@Column(name="depart_campus", nullable=true, length=45)	
+	private String depart_campus;
+	
+	@Column(name="arrive_campus", nullable=true, length=45)	
+	private String arrive_campus;
+	
+	@OneToMany(mappedBy="trip_idtrip", targetEntity=com.suptrips.Users.class)	
+
+	private java.util.Set users = new java.util.HashSet();
+	
+	private void setIdtrip(int value) {
+		this.idtrip = value;
+	}
+	
+	public int getIdtrip() {
+		return idtrip;
+	}
+	
+	public void setDepart_date(java.util.Date value) {
+		this.depart_date = value;
+	}
+	
+	public java.util.Date getDepart_date() {
+		return depart_date;
+	}
+	
+	public void setArrive_date(java.util.Date value) {
+		this.arrive_date = value;
+	}
+	
+	public java.util.Date getArrive_date() {
+		return arrive_date;
+	}
+	
+	public void setDepart_campus(String value) {
+		this.depart_campus = value;
+	}
+	
+	public String getDepart_campus() {
+		return depart_campus;
+	}
+	
+	public void setArrive_campus(String value) {
+		this.arrive_campus = value;
+	}
+	
+	public String getArrive_campus() {
+		return arrive_campus;
+	}
+	
+	public void setUsers(java.util.Set value) {
+		this.users = value;
+	}
+	
+	public java.util.Set getUsers() {
+		return users;
+	}
+	
+	
+	public String toString() {
+		return String.valueOf(getIdtrip());
+	}
+	
+}
---
+++ src/com/suptrips/Users.java
@@ -0,0 +1,101 @@
+/**
+ * "Visual Paradigm: DO NOT MODIFY THIS FILE!"
+ * 
+ * This is an automatic generated file. It will be regenerated every time 
+ * you generate persistence class.
+ * 
+ * Modifying its content may cause the program not work, or your work may lost.
+ */
+
+/**
+ * Licensee: 
+ * License Type: Evaluation
+ */
+package com.suptrips;
+
+import java.io.Serializable;
+import javax.persistence.*;
+@Entity
+
+@Table(name="Users")
+public class Users implements Serializable {
+	public Users() {
+	}
+	
+	@Column(name="idbooster", nullable=false, length=11)	
+	@Id	
+	@GeneratedValue(generator="COM_SUPTRIPS_USERS_IDBOOSTER_GENERATOR")	
+
+	private int idbooster;
+	
+	@Column(name="firstname", nullable=true, length=45)	
+	private String firstname;
+	
+	@Column(name="lastname", nullable=true, length=45)	
+	private String lastname;
+	
+	@Column(name="email", nullable=true, length=45)	
+	private String email;
+	
+	@Column(name="campus_name", nullable=true, length=45)	
+	private String campus_name;
+	
+	@ManyToOne(targetEntity=Trips.class)
+
+	@JoinColumns({ @JoinColumn(name="Trip_idtrip", referencedColumnName="idtrip", nullable=false) })	
+
+	private Trips trip_idtrip;
+	
+	private void setIdbooster(int value) {
+		this.idbooster = value;
+	}
+	
+	public int getIdbooster() {
+		return idbooster;
+	}
+	
+	public void setFirstname(String value) {
+		this.firstname = value;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setLastname(String value) {
+		this.lastname = value;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	
+	public void setEmail(String value) {
+		this.email = value;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setCampus_name(String value) {
+		this.campus_name = value;
+	}
+	
+	public String getCampus_name() {
+		return campus_name;
+	}
+	
+	public void setTrip_idtrip(Trips value) {
+		this.trip_idtrip = value;
+	}
+	
+	public Trips getTrip_idtrip() {
+		return trip_idtrip;
+	}
+	
+	public String toString() {
+		return String.valueOf(getIdbooster());
+	}
+	
+}
